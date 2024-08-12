141de763e87aa97f39c5d4f4089cf3156579fcd2
Sergey Breusov <bsa2609@yandex.ru>
2024-08-12 21:50:19 +0300

    edit tests

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -43,8 +43,7 @@ public class App implements Callable<Integer> {
     }
 
     public static void main(String[] args) {
-        //int exitCode = new CommandLine(new App()).execute(args);
-        //System.exit(exitCode);
-        new CommandLine(new App()).execute(args);
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
